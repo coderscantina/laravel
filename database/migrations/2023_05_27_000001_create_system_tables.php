2d1524f91c1af7bc0cefbd4cca8e924294c529e8
Michael Wallner <m.wallner@neonblack.at>
2024-01-18 20:24:48 +0100

    🗃️ enhance migrations

--- database/migrations/2023_05_27_000001_create_system_tables.php
+++ database/migrations/2023_05_27_000001_create_system_tables.php
@@ -4,44 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
+
     public function up(): void
     {
         Schema::create('countries', function (Blueprint $table) {
             $table->char('iso2', 2)->charset('ascii')->collation('ascii_bin')->primary();
 
             $table->string('name_local')->nullable();
-            $table->string('name_ar')->nullable();
-            $table->string('name_cs')->nullable();
-            $table->string('name_da')->nullable();
-            $table->string('name_de')->nullable();
-            $table->string('name_en')->nullable();
-            $table->string('name_es')->nullable();
-            $table->string('name_fi')->nullable();
-            $table->string('name_fr')->nullable();
-            $table->string('name_el')->nullable();
-            $table->string('name_hr')->nullable();
-            $table->string('name_it')->nullable();
-            $table->string('name_ja')->nullable();
-            $table->string('name_ka')->nullable();
-            $table->string('name_ko')->nullable();
-            $table->string('name_nl')->nullable();
-            $table->string('name_no')->nullable();
-            $table->string('name_pl')->nullable();
-            $table->string('name_pt')->nullable();
-            $table->string('name_pt_br')->nullable();
-            $table->string('name_ro')->nullable();
-            $table->string('name_ru')->nullable();
-            $table->string('name_sk')->nullable();
-            $table->string('name_sl')->nullable();
-            $table->string('name_sv')->nullable();
-            $table->string('name_tr')->nullable();
-            $table->string('name_uk')->nullable();
-            $table->string('name_zh')->nullable();
+            $table->json('name_translation')->nullable();
 
             $table->timestamps();
             $table->softDeletes();
@@ -63,9 +34,6 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('countries');
