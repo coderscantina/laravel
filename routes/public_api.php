82e56ef792a76bee21730fe9376942ad0b30461a
Michael Wallner <m.wallner@neonblack.at>
2024-01-18 20:23:33 +0100

    ✨ introduce a config controller

--- routes/public_api.php
+++ routes/public_api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\ConfigController;
 use App\Http\Controllers\Api\HealthController;
 
 Route::get('health', HealthController::class)->name('health');
+Route::get('config', ConfigController::class)->name('config');
