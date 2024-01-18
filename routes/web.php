f019b33f7e8916577d5331c3158f6304bea3f740
Michael Wallner <m.wallner@neonblack.at>
2024-01-18 20:46:22 +0100

    🛣️ add missing route

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,5 @@ use App\Http\Controllers\PassThroughController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/login', PassThroughController::class)->name('login');
+Route::get('/password/reset', PassThroughController::class)->name('password.request');
 Route::get('/password/reset/{token}', PassThroughController::class)->name('password.reset');
