2b149df26b37c218f6f9a4dcbdb248d9b1f43a01
Michael Wallner <m.wallner@neonblack.at>
2024-01-18 20:24:06 +0100

    ✨ expose translations via an endpoint

--- routes/public_api.php
+++ routes/public_api.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\Api\ConfigController;
 use App\Http\Controllers\Api\HealthController;
+use App\Http\Controllers\Api\TranslationController;
 
 Route::get('health', HealthController::class)->name('health');
 Route::get('config', ConfigController::class)->name('config');
+Route::get('translations', TranslationController::class)->name('translations');
