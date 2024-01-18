ad2106f8bd77e54f38541c2e31c2bec41c0f0d2d
Michael Wallner <m.wallner@neonblack.at>
2024-01-18 20:55:22 +0100

    ✨ add settings endpoint for users

--- routes/private_api.php
+++ routes/private_api.php
@@ -2,11 +2,13 @@
 
 use App\Http\Controllers\Api\User\UserController as UserControllerAlias;
 use App\Http\Controllers\Api\User\UserPasswordController;
+use App\Http\Controllers\Api\User\UserSettingsController;
 
 Route::group(['prefix' => 'users'], function () {
 
     Route::group(['prefix' => 'me'], function () {
         Route::get('/', [UserControllerAlias::class, 'show'])->name('users.me.show');
+        Route::post('/settings', UserSettingsController::class)->name('users.me.settings');
 
         Route::post('/password', UserPasswordController::class)
             ->middleware(['throttle:crucial'])
