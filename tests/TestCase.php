e57800d235430dbd735e9c23a5c77d4ea358511f
Michael Wallner <m.wallner@neonblack.at>
2024-01-18 20:46:03 +0100

    👔 add helper for tests to setup user and actAs

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,21 @@
 
 namespace Tests;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected User $user;
+
+    protected function createAndActAs(?User $user = null): void
+    {
+        if (!$user) {
+            $user = User::factory()->create();
+        }
+        $this->user = $user;
+        $this->actingAs($this->user);
+    }
 }
