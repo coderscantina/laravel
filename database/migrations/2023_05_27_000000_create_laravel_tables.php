679a9a5b453546054b6f23ce4ccc1f79ca171de7
Michael Wallner <m.wallner@neonblack.at>
2025-03-10 21:40:33 +0100

    🗃️ add laravel related system tables

--- database/migrations/2023_05_27_000000_create_laravel_tables.php
+++ database/migrations/2023_05_27_000000_create_laravel_tables.php
@@ -7,6 +7,18 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void
     {
+        Schema::create('cache', function (Blueprint $table) {
+            $table->string('key')->primary();
+            $table->mediumText('value');
+            $table->integer('expiration');
+        });
+
+        Schema::create('cache_locks', function (Blueprint $table) {
+            $table->string('key')->primary();
+            $table->string('owner');
+            $table->integer('expiration');
+        });
+
         Schema::create('features', function (Blueprint $table) {
             $table->id();
             $table->string('name');
@@ -57,5 +69,7 @@ return new class extends Migration {
         Schema::dropIfExists('failed_jobs');
         Schema::dropIfExists('jobs');
         Schema::dropIfExists('features');
+        Schema::dropIfExists('cache_locks');
+        Schema::dropIfExists('cache');
     }
 };
--- database/migrations/2023_05_27_000002_create_user_tables.php
+++ database/migrations/2023_05_27_000002_create_user_tables.php
@@ -51,10 +51,20 @@ return new class extends Migration
 
             $table->timestamps();
         });
+
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
+        });
     }
 
     public function down(): void
     {
+        Schema::dropIfExists('sessions');
         Schema::dropIfExists('user_social_links');
         Schema::dropIfExists('password_reset_tokens');
         Schema::dropIfExists('users');
